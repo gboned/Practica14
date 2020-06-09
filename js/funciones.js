15d03f65bfe2bdecdd3a9a21eb11b6da69efaa5c
gboned <gboned@cifpfbmoll.eu>
2020-06-10 00:04:45 +0200

    Añadir función que cambie las medidas de imagen

--- js/funciones.js
+++ js/funciones.js
@@ -7,3 +7,23 @@
 // Creo variable global vacía.
 var imagenAgrandada;
 
+// Creo función que cambie la medida de la imagen.
+function cambiarMedidasImagen() {
+
+    // Creo variable que guarde la imagen.
+    var imagen = document.getElementById("imagen");
+    
+    // Añado listener que se ejecuta al hacer click en la imagen.
+    imagen.addEventListener("click", function() {
+        // Si la variable imagenAgrandada es true o existe, cambia a false y hace pequeña la imagen.
+        if (imagenAgrandada) {
+            imagenAgrandada = false;
+            this.width = 400;
+        } else {
+            // Si es false, el tamaño aumenta al hacer click.
+            this.width = 700;
+            imagenAgrandada = true;
+        }
+    })
+
+}
